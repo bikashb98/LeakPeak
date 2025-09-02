015d0a2aa565f5cca10f6a4fcfd0fb8991f3af9a
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-09-02 13:00:37 +0530

    Initialized storybook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <div>
-      hi there
+    <div className="h-screen flex justify-center bg-gradient-to-br from-blue-950 via-gray-900 to-gray-950 border border-red-500">
+      <div className="flex items-center border border-bg-cyan-500 w-lg">
+        <div className="flex w-full">
+          <Input className= 'h-13 bg-amber-50' placeholder = 'Email address' />
+        </div>
+      </div>
     </div>
   );
 }
