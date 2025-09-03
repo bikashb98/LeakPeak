2216f9a1c99516e1ceb0b012f955859964654293
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-09-04 00:39:04 +0530

    Built the DateLine component in isolation with storybook

--- components/DateLine.tsx
+++ components/DateLine.tsx
@@ -1,8 +1,8 @@
 export function DateLine({ date }: { date: string }) {
     return (
-        <div className="flex h-screen items-center">
-            <div className="w-1/2 border-r border-blue-800"></div>
-            
+        <div className="flex justify-center h-150 mx-auto relative">
+            <div className="h-full border-r border-blue-800 absolute  "></div>
+            <div className="my-8 w-15 h-15 rounded-full bg-blue-700 flex items-center justify-center text-white text-sm font-bold relative border-2 border-blue-200 shadow-lg">{date}</div>
         </div>
     )
 }
